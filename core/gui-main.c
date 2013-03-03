4cf14d618cf32d7f7ca62dcd10f61f0274eaae87
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-03 15:23:41 +0100

    gui: add menu
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- core/gui-main.c
+++ core/gui-main.c
@@ -49,8 +49,8 @@ static void setup_main_row_artwork_image(struct ps *ps)
 
         gtk_box_pack_start(GTK_BOX(ps->s.vbox), event_box, FALSE, TRUE, 0);
         gtk_widget_show_all(event_box);
-
 }
+
 
 static GdkScreen* get_default_screen(void)
 {
@@ -68,6 +68,93 @@ static void resize_main_window(GtkWidget *window)
 	height = gdk_screen_get_height(screen);
 
 	gtk_window_resize(GTK_WINDOW(window), width * 3 / 4, height  * 3 / 4);
+}
+
+static void setup_menu(struct ps *ps)
+{
+	GtkWidget *menubar;
+	GtkWidget *filemenu;
+	GtkWidget *projectmenu;
+	GtkWidget *systemmenu;
+	GtkWidget *viewmenu;
+	GtkWidget *helpmenu;
+
+	GtkWidget *file;
+	GtkWidget *file_quit;
+
+	GtkWidget *view;
+	GtkWidget *view_report;
+	GtkWidget *project_recent;
+	GtkWidget *project_manage;
+
+	GtkWidget *project;
+	GtkWidget *project_report;
+
+	GtkWidget *systemm;
+	GtkWidget *system_report;
+
+	GtkWidget *help;
+	GtkWidget *help_report;
+
+	menubar     = gtk_menu_bar_new();
+	gtk_widget_set_name(menubar, "menubar");
+
+	filemenu    = gtk_menu_new();
+	projectmenu = gtk_menu_new();
+	systemmenu  = gtk_menu_new();
+	viewmenu    = gtk_menu_new();
+	helpmenu    = gtk_menu_new();
+
+	/* File submenues */
+	file      = gtk_menu_item_new_with_mnemonic("_File");
+	file_quit = gtk_image_menu_item_new_from_stock(GTK_STOCK_QUIT, NULL);
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(file), filemenu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), file_quit);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), file);
+
+	/* Projects submenues */
+	project         = gtk_menu_item_new_with_mnemonic("_Projects");
+	project_report  = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+	project_recent  = gtk_menu_item_new_with_mnemonic("Re_cent Projects");
+	project_manage  = gtk_menu_item_new_with_mnemonic("_Manage Projects");
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(project), projectmenu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(projectmenu), project_report);
+	gtk_menu_shell_append(GTK_MENU_SHELL(projectmenu), project_recent);
+	gtk_menu_shell_append(GTK_MENU_SHELL(projectmenu), project_manage);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), project);
+
+	/* System submenues */
+	systemm         = gtk_menu_item_new_with_mnemonic("_Systems");
+	system_report  = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(systemm), systemmenu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(systemmenu), system_report);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), systemm);
+
+	/* View submenues */
+	view         = gtk_menu_item_new_with_mnemonic("_View");
+	view_report  = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(view), viewmenu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(viewmenu), view_report);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), view);
+
+	/* Help submenues */
+	help        = gtk_menu_item_new_with_mnemonic("_Help");
+	help_report  = gtk_image_menu_item_new_from_stock(GTK_STOCK_NEW, NULL);
+
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(help), helpmenu);
+	gtk_menu_shell_append(GTK_MENU_SHELL(helpmenu), help_report);
+	gtk_menu_shell_append(GTK_MENU_SHELL(menubar), help);
+
+	gtk_box_pack_start(GTK_BOX(ps->s.vbox), menubar, FALSE, FALSE, 0);
+	gtk_widget_show_all(menubar);
+
+	g_signal_connect(G_OBJECT(file_quit), "activate", G_CALLBACK(gtk_main_quit), NULL);
+
+	return;
 }
 
 
@@ -90,6 +177,7 @@ int gui_init(struct ps *ps, int ac, char **av)
 
 	setup_main_row_layout(ps);
 	setup_main_row_artwork_image(ps);
+	setup_menu(ps);
 
 	resize_main_window(ps->s.main_window);
 	gtk_window_set_position(GTK_WINDOW(ps->s.main_window), GTK_WIN_POS_CENTER);
