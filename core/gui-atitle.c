8d973b07de3a30122ade42dea7f72312691601fb
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-03 16:04:39 +0100

    gui: add tooltip support
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- core/gui-atitle.c
+++ core/gui-atitle.c
@@ -12,6 +12,7 @@ void gui_atitle_set_title(struct ps *ps, const gchar *title)
 
 void gui_atitle_set_tooltip(struct ps *ps, const gchar *tooltip)
 {
+	gtk_widget_set_tooltip_text(ps->s.atitle.label, tooltip);
 }
 
 
@@ -24,6 +25,9 @@ void gui_atitle_init(struct ps *ps)
 	hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
 	ps->s.atitle.label = gtk_label_new(NULL);
 	gui_atitle_set_title(ps, "Project Foo");
+	gui_atitle_set_tooltip(ps, "\nDescription: nulla shizzlin dizzle Its fo rizzle faucibizzle pharetra\n"
+			"Path: /usr/src/linux\n"
+			"Args:\n");
 	gtk_box_pack_end(GTK_BOX(hbox), ps->s.atitle.label, FALSE, FALSE, 0);
 
 	gtk_box_pack_start(GTK_BOX(ps->s.vbox), hbox, FALSE, FALSE, 0);
