70522eb4e11352469a12f552d5c271cdea12ebc5
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-08 21:14:05 +0100

    core: add string parser
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

---
+++ core/str-parser.c
@@ -0,0 +1,147 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <assert.h>
+
+#include "str-parser.h"
+
+
+void str_parser_init(struct str_parser *str_parser, const char *new_str)
+{
+	assert(str_parser);
+	assert(new_str);
+	assert(*new_str);
+
+	memset(str_parser, 0, sizeof(*str_parser));
+	str_parser->start_ptr = new_str;
+	str_parser->curr_ptr  = new_str;
+}
+
+
+void str_parser_reset(struct str_parser *str_parser)
+{
+	assert(str_parser);
+	str_parser->curr_ptr = str_parser->start_ptr;
+}
+
+
+int str_parser_skip_spaces(struct str_parser *str_parser)
+{
+	assert(str_parser);
+
+	if (!*str_parser->curr_ptr)
+		return STR_PARSER_RET_EOL;
+
+	while (isspace(*str_parser->curr_ptr))
+		str_parser->curr_ptr++;
+
+	if (!*str_parser->curr_ptr)
+		return STR_PARSER_RET_EOL;
+
+	return STR_PARSER_RET_SUCCESS;
+}
+
+
+int str_parser_long(struct str_parser *str_parser, long *retval)
+{
+	long val;
+	char *endptr;
+
+	assert(str_parser);
+
+	if (!*str_parser->curr_ptr)
+		return STR_PARSER_RET_EOL;
+
+	errno = 0;
+	val = strtol(str_parser->curr_ptr, &endptr, 10);
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		return STR_PARSER_RET_EXCEPTION;
+	}
+
+	if (endptr == str_parser->curr_ptr) {
+		fprintf(stderr, "No digits were found\n");
+		return STR_PARSER_RET_INVALID;
+	}
+
+	*retval = val;
+	str_parser->curr_ptr = endptr;
+
+	return STR_PARSER_RET_SUCCESS;
+}
+
+
+int str_parser_next_long(struct str_parser *str_parser, long *retval)
+{
+	int ret;
+
+	assert(str_parser);
+
+	/* skip potential leading whitespaced */
+	ret = str_parser_skip_spaces(str_parser);
+	if (ret != STR_PARSER_RET_SUCCESS)
+		return ret;
+
+	ret = str_parser_long(str_parser, retval);
+	if (ret != STR_PARSER_RET_SUCCESS)
+		return ret;
+
+	return STR_PARSER_RET_SUCCESS;
+}
+
+
+int str_parser_next_alphanum(struct str_parser *str_parser, char *dest, size_t max_len)
+{
+	int ret;
+	ptrdiff_t delta;
+	const char *newptr;
+
+	/* skip potential leading whitespaced */
+	ret = str_parser_skip_spaces(str_parser);
+	if (ret != STR_PARSER_RET_SUCCESS)
+		return ret;
+
+	newptr = str_parser->curr_ptr;
+
+	while (isalnum(*newptr)) {
+		newptr++;
+	}
+
+	if (newptr == str_parser->curr_ptr)
+		return STR_PARSER_RET_INVALID;
+
+	delta = newptr - str_parser->curr_ptr;
+
+	if (delta > (ptrdiff_t)max_len - 1) {
+		return STR_PARSER_RET_NOBUFS;
+	}
+
+	memcpy(dest, str_parser->curr_ptr, delta);
+	dest[delta] = '\0';
+
+	str_parser->curr_ptr = newptr;
+
+	return STR_PARSER_RET_SUCCESS;
+}
+
+
+int str_parser_remain(struct str_parser *str_parser, char *dest, size_t max_len)
+{
+	size_t delta;
+
+	delta = strlen(str_parser->curr_ptr);
+	if (delta > max_len -1)
+		return STR_PARSER_RET_NOBUFS;
+
+	memcpy(dest, str_parser->curr_ptr, delta);
+	dest[delta] = '\0';
+
+	str_parser->curr_ptr += delta;
+
+	return STR_PARSER_RET_SUCCESS;
+}
---
+++ core/str-parser.h
@@ -0,0 +1,25 @@
+#ifndef STR_PARSER_H
+#define STR_PARSER_H
+
+#define STR_PARSER_RET_SUCCESS    0
+#define STR_PARSER_RET_EOL       -1
+#define STR_PARSER_RET_INVALID   -2
+#define STR_PARSER_RET_EXCEPTION -3
+#define STR_PARSER_RET_NOBUFS    -4
+
+
+struct str_parser {
+	const char const *start_ptr;
+	const char *curr_ptr;
+};
+
+void str_parser_init(struct str_parser *str_parser, const char *new_str);
+void str_parser_reset(struct str_parser *str_parser);
+int str_parser_skip_spaces(struct str_parser *str_parser);
+int str_parser_long(struct str_parser *str_parser, long *retval);
+int str_parser_next_long(struct str_parser *str_parser, long *retval);
+int str_parser_next_alphanum(struct str_parser *str_parser, char *dest, size_t max_len);
+int str_parser_remain(struct str_parser *str_parser, char *dest, size_t max_len);
+
+
+#endif /* STR_PARSER_H */
