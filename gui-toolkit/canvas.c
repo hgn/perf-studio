cb406fd1eeb06dcf13be9dd18c4ad839fd36cfd8
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-10 17:42:53 +0100

    gui-toolkit: add error dialog
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- gui-toolkit/canvas.c
+++ gui-toolkit/canvas.c
@@ -13,3 +13,22 @@ GtkWidget *gt_stub_widget(struct ps *ps, const gchar *str, guint width, guint he
 
 	return frame;
 }
+
+static void screen_err_msg(const gchar *format, ...)
+{
+	va_list  args;
+	gchar *str;
+	GtkWidget *dialog;
+
+	va_start(args, format);
+	str = g_strdup_vprintf (format, args);
+	va_end(args);
+
+	dialog = gtk_message_dialog_new(GTK_WINDOW(sc.screen.main_window), 0,
+				        GTK_MESSAGE_ERROR, GTK_BUTTONS_OK, "%s", str);
+
+	gtk_dialog_run(GTK_DIALOG(dialog));
+	g_free(str);
+	gtk_widget_destroy(dialog);
+}
+
