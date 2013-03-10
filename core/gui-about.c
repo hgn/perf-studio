6a32b091cc526d0dfbab7128e25b77bc91910b2d
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-10 22:31:15 +0100

    gui: add logo for about dialog
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- core/gui-about.c
+++ core/gui-about.c
@@ -2,14 +2,22 @@
 #include "version.h"
 #include "authors.h"
 
+#define PERF_LOGO_FILENAME "perf-studio-logo.png"
+
 void gui_show_about(GtkWidget *widget, struct ps *ps)
 {
+	gchar *logo_path;
+	GdkPixbuf *pixbuf;
 	GtkWidget *about_dialog;
 
 	(void) widget;
-	(void) ps;
+
+	logo_path = g_build_filename(ps->si.pixmapdir, PERF_LOGO_FILENAME, NULL);
+	fprintf(stderr, "%s\n", logo_path);
+	pixbuf = gdk_pixbuf_new_from_file(logo_path, NULL);
 
 	about_dialog = gtk_about_dialog_new();
+	gtk_about_dialog_set_logo(GTK_ABOUT_DIALOG(about_dialog), pixbuf);
 	gtk_about_dialog_set_version(GTK_ABOUT_DIALOG(about_dialog), VERSION_STRING);
 	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about_dialog), "(C) 2013 Hagen Paul Pfeifer <hagen@jauu.net>");
 	gtk_about_dialog_set_website(GTK_ABOUT_DIALOG(about_dialog), "http://perf-studio.com");
@@ -17,7 +25,11 @@ void gui_show_about(GtkWidget *widget, struct ps *ps)
 	gtk_about_dialog_set_license_type(GTK_ABOUT_DIALOG(about_dialog), GTK_LICENSE_GPL_3_0);
 
 
+	g_free(logo_path);
+	g_object_unref(pixbuf);
+	gtk_window_set_position(GTK_WINDOW(about_dialog), GTK_WIN_POS_CENTER);
 	gtk_dialog_run(GTK_DIALOG (about_dialog));
 	gtk_widget_destroy(about_dialog);
+
 }
 
