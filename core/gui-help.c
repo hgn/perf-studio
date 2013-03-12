530c255056f4e866c81fb291df7c0ab2142e0934
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-12 19:57:08 +0100

    gui: fix webkit help warnings
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- core/gui-help.c
+++ core/gui-help.c
@@ -1,11 +1,14 @@
 #include <webkit/webkit.h>
+#include <string.h>
 
 #include "gui-help.h"
 #include "shared.h"
 
 
-static gboolean close_webview_cb(WebKitWebView* webView, GtkWidget* window)
+static gboolean close_webview_cb(WebKitWebView* web_view, GtkWidget* window)
 {
+	(void) web_view;
+
 	gtk_widget_destroy(window);
 	return TRUE;
 }
@@ -16,7 +19,11 @@ void gui_help_overview_window(GtkWidget *widget, struct ps *ps)
 {
 	gchar *html_help_path;
 	GtkWidget *help_window;
+	GtkWidget *scroll_window;
+	WebKitWebView *web_view;
 	gchar *url;
+
+	(void)widget;
 
 	html_help_path = g_build_filename(DATA_DIR, "help-pages/index.html", NULL);
 	url = g_malloc(strlen(html_help_path) + strlen(FILE_URI_SCHEME) + 2);
@@ -28,16 +35,17 @@ void gui_help_overview_window(GtkWidget *widget, struct ps *ps)
 
 	gtk_window_set_default_size(GTK_WINDOW(help_window), 800, 600);
 
-	WebKitWebView *webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
-	GtkWidget *scrolledWindow = gtk_scrolled_window_new(NULL, NULL);
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledWindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_container_add(GTK_CONTAINER(scrolledWindow), GTK_WIDGET(webView));
+	web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+	scroll_window = gtk_scrolled_window_new(NULL, NULL);
 
-	g_signal_connect(webView, "close-web-view", G_CALLBACK(close_webview_cb), help_window);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll_window), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_container_add(GTK_CONTAINER(scroll_window), GTK_WIDGET(web_view));
 
-	gtk_container_add(GTK_CONTAINER(help_window), scrolledWindow);
-	webkit_web_view_load_uri(webView, url);
-	gtk_widget_grab_focus(GTK_WIDGET(webView));
+	g_signal_connect(web_view, "close-web-view", G_CALLBACK(close_webview_cb), help_window);
+
+	gtk_container_add(GTK_CONTAINER(help_window), scroll_window);
+	webkit_web_view_load_uri(web_view, url);
+	gtk_widget_grab_focus(GTK_WIDGET(web_view));
 
 	gtk_window_set_decorated(GTK_WINDOW(help_window), FALSE);
 	gtk_window_set_position((GtkWindow *)help_window, GTK_WIN_POS_CENTER);
