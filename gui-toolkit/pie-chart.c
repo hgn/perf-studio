46889d36282e41a45dfcab0d551a04e0f64ddb7a
Hagen Paul Pfeifer <hagen@jauu.net>
2013-03-17 15:06:13 +0100

    pie-chart: save tmp data into g_array
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- gui-toolkit/pie-chart.c
+++ gui-toolkit/pie-chart.c
@@ -20,7 +20,8 @@ struct gt_pie_chart *gt_pie_chart_new(void)
         struct gt_pie_chart *gtpc;
 
         gtpc = g_malloc(sizeof(*gtpc));
-        gtpc->pie_data_slot_array = g_array_new(FALSE, FALSE, sizeof(struct pie_data_slot));
+        gtpc->pie_data_slot_array = g_array_new(FALSE, FALSE,
+						sizeof(struct pie_data_slot));
         return gtpc;
 }
 
@@ -35,29 +36,33 @@ void gt_pie_chart_free(struct gt_pie_chart *gt_pie_chart)
 }
 
 
-void gt_pie_chart_set_linewidth(struct gt_pie_chart *gt_pie_chart, int line_width)
+void gt_pie_chart_set_linewidth(struct gt_pie_chart *gt_pie_chart,
+				int line_width)
 {
         gt_pie_chart->line_width = line_width;
 }
 
 
-void gt_pie_chart_set_offsets(struct gt_pie_chart *gt_pie_chart, int x_offset, int y_offset)
+void gt_pie_chart_set_offsets(struct gt_pie_chart *gt_pie_chart,
+			      int x_offset, int y_offset)
 {
         gt_pie_chart->xo = x_offset;
         gt_pie_chart->yo = y_offset;
 }
 
-void gt_pie_chart_set_radius(struct gt_pie_chart *gt_pie_chart, int inner_radius, int outer_radius)
+void gt_pie_chart_set_radius(struct gt_pie_chart *gt_pie_chart,
+			     int inner_radius, int outer_radius)
 {
 	gt_pie_chart->inner_radius = inner_radius;
 	gt_pie_chart->outer_radius = outer_radius;
 }
 
 
-void gt_pie_chart_set_data(struct gt_pie_chart *gt_pie_chart, struct kv_list *chart_data_list)
+void gt_pie_chart_set_data(struct gt_pie_chart *gt_pie_chart,
+			   struct kv_list *chart_data_list)
 {
+	int elements, i, j;
 	long sum, max;
-	int elements, i;
 	float *percentages;
 	float *angles;
 	GSList *tmp;
@@ -81,19 +86,42 @@ void gt_pie_chart_set_data(struct gt_pie_chart *gt_pie_chart, struct kv_list *ch
 	percentages = g_malloc(sizeof(float) * elements);
 	angles      = g_malloc(sizeof(float) * elements);
 
+	j = 0;
 	tmp = KV_LIST_HEAD(chart_data_list);
 	while (tmp) {
+		struct pie_data_slot *pie_data_slot;
 		struct kv_list_entry *entry;
+
 		entry = tmp->data;
 		assert(entry);
 
                 percentages[i] = ((float)GPOINTER_TO_INT(entry->key) / sum) * 360.0;
                 angles[i] = percentages[i] * (M_PI / 180.0);
 
-		fprintf(stderr, "angle: %f\n",  percentages[i]);
+
+		if (gt_pie_chart->pie_data_slot_array->len <= j)
+			g_array_set_size(gt_pie_chart->pie_data_slot_array, j + 1);
+
+		pie_data_slot = &g_array_index(gt_pie_chart->pie_data_slot_array,
+					       struct pie_data_slot, j);
+		pie_data_slot->angle = angles[i];
+		memcpy(pie_data_slot->label, entry->value, sizeof(pie_data_slot->label));
+		pie_data_slot->label[sizeof(pie_data_slot->label) - 1] = '\0';
+
+		fprintf(stderr, "angle: %f %s\n",  angles[i], pie_data_slot->label);
 
 		tmp = g_slist_next(tmp);
+		j++;
 	}
+
+	/*
+	 * FIXME: should it possible that the array must be reduced to
+	 * chart_data_list length? This implementation assume that the
+	 * chart_data_list is never reduced during runtime! I.e. a four
+	 * segment pie chart will always have four segments. This may change
+	 * in future because some pie charts user may have other requirements,
+	 * but this is up to the future.
+	 */
 
 	g_free(percentages);
 	g_free(angles);
