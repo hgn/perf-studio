5437926c7f5d2223bd00ee0c37d599665eb3fec8
Hagen Paul Pfeifer <hagen@jauu.net>
2014-03-03 23:14:49 +0000

    logger: default to LOG_INFO
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- include/log.h
+++ include/log.h
@@ -5,7 +5,6 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdarg.h>
-#include <time.h>
 
 #define LOG_CRITICAL 0
 #define LOG_ERROR    1
@@ -21,12 +20,12 @@
 void log_set_logfile(char *);
 int log_get_mode();
 void log_set_mode(int);
+void logger_set_level(int level);
 
 #define log_print(level, ...) do {				\
-	time_t _clk = time(NULL);	\
-	_print_log(level, __FILE__, __func__, ctime(&_clk), __LINE__, __VA_ARGS__); \
+	_print_log(level, __FILE__, __func__, __LINE__, __VA_ARGS__); \
 } while (0)
 
-extern void _print_log(int, const char *, const char *, const char *, int, const char *, ...);
+extern void _print_log(int, const char *, const char *, int, const char *, ...);
 
 #endif
