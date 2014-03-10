7e543362bc6878fc680eadfd3a6172941c31563e
Hagen Paul Pfeifer <hagen@jauu.net>
2014-03-10 23:17:03 +0100

    core: remove unused variable
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- core/executer.c
+++ core/executer.c
@@ -37,7 +37,6 @@ static int execute_measurement(struct executer_gui_ctx *executer_gui_ctx,
         int ret;
 	char buffer[1024];
 	gchar *str;
-	char *newenviron[] = { NULL };
 
 	ret = 0;
 
@@ -69,8 +68,6 @@ static int execute_measurement(struct executer_gui_ctx *executer_gui_ctx,
 		setvbuf(stdout, NULL, _IOLBF, 0);
 		setvbuf(stderr, NULL, _IOLBF, 0);
 
-		//execv(cmd[0], (char * const*)cmd);
-		fprintf(stderr, "CMD: %s\n", cmd[0]);
 		execve(cmd[0], cmd, environ);
 		log_print(LOG_ERROR, "Failed to execute program: %s", strerror(errno));
 		exit(100);
